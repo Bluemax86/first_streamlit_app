cdc9900d34f76e6279dd7a54f1a8ec5895f21fea
Bluemax86 <Bluemax86@yahoo.com>
2022-06-29 12:03:10 -0500

    Update streamlit_app.py
    convert the json data into a table format and display that data

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ import requests
 streamlit.header('Fruityvice Fruit Advice')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Normalize the json data into a flat table format 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# put the normalized data into a table format
+streamlit.dataframe(fruityvice_normalized)
